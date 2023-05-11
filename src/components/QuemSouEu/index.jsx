8cf846fb04f47fd9631fc3c8a794d2b68e86e2a1
Athos Iury Alexandre de Moraes Gonçalves <88007943+AthosAlexandre@users.noreply.github.com>
2023-05-11 10:16:31 -0400

    testando

--- src/components/QuemSouEu/index.jsx
+++ src/components/QuemSouEu/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function QuemSouEu() {
   return (
     <section>
-        Olá, sou Athos Alexandre, sou estudante de Sistemas de Informação na Universidade Universidade Federal do Mato Grosso do sul campos Coxim.
+        Olá, sou Athos Alexandre, sou estudante de Sistemas de Informação na Universidade Universidade Federal do Mato Grosso do sul campos Coxim.aa
     </section>
   )
 }
