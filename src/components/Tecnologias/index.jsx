e9d4c94de9ff2ccd7b3c027d1651ed41ea3ddf5a
Athos Iury Alexandre de Moraes Gonçalves <88007943+AthosAlexandre@users.noreply.github.com>
2023-05-11 15:55:41 -0400

    Colocando conteudo no componente de tecnologia

---
+++ src/components/Tecnologias/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Tecnologias.css'
+
+function Tecnologias() {
+  return (
+    <section className='tecnologias'>
+        <h3>MINHAS SKILLS</h3>
+        <div className="container-tecnologias">
+            <div className="react"></div>
+            <div className="vue"></div>
+            <div className="java"></div>
+            <div className="mysql"></div>
+            <div className="bootstrap"></div>
+            <div className="gitHub"></div>
+        </div>
+    </section>
+  )
+}
+
+export default Tecnologias
