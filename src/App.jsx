dc45a0d74363aae951756445848767ca46f20236
Athos Iury Alexandre de Moraes Gonçalves <88007943+AthosAlexandre@users.noreply.github.com>
2023-05-12 09:08:09 -0400

    Adicionando o componente OqueEuFaco e ajustando o componente da Tecnologia

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 
 import Home from './components/Home'
+import OqueEuFaco from './components/OqueEuFaco'
 import Tecnologias from './components/Tecnologias'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <>
       <Home/>
       <Tecnologias/>
+      <OqueEuFaco/>
     </>
   )
 }
---
+++ src/components/OqueEuFaco/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './OqueEuFaco.css'
+function OqueEuFaco() {
+  return (
+    <div className='oque-eu-faco'>OqueEuFaco</div>
+  )
+}
+
+export default OqueEuFaco
